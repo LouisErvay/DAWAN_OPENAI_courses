ed1c5ba09c62d56445a338f72a5fef294c9c0130
Louis E <enjules31@gmail.com>
2024-12-17 10:34:44 +0100

    MAJ chatbot avec AI

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import streamlit as st
 OPENAI_API_KEY = os.getenv("api_key")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-completion = client.chat.completions.create(
-    model = "gpt-4o-mini",
-    messages = [
-        {"role" : "user", "content" : "Hello world"}
-    ]
-)
+value = st.chat_input("Votre message ici")
 
-print(completion)
+if value:
+    with (st.chat_message("user")):
+        st.write(value)
+    
+    with (st.chat_message("assistant")):
+        txt = st.header("Waiting for response...")
 
-st.write("Hello world")
+        completion = client.chat.completions.create(
+            model = "gpt-4o-mini",
+            messages = [
+                {"role" : "user", "content" : value}
+            ]
+        )
+
+        txt.text(completion.choices[0].message.content)
